bf372eabb9181f48e26ea6ff7571de2b85db2d8a
martin-dx <martin-dx@martin-dx.kali>
2020-12-18 11:40:07 +0100

    add job urls & views

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job',
     'djmoney',
+
+    # my apps
+    'job',
+    'blog',
+    'contact',
+    'login',
+    'home',
 ]
 
 MIDDLEWARE = [
