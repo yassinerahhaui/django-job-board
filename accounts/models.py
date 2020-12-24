5d85669d3d666230cc53e02c8574515fc380902d
martin-dx <martin-dx@dhcppc6.martin-dx>
2020-12-24 16:26:45 +0100

    add profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
 
-# Create your models here.
+def image_upload(instance,filename):
+    imagename, extansion = filename.split('.')
+    return f'profile/{uuid.uuid4()}.{extansion}'
+
+# Create your models here.######################
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    city = models.ForeignKey('City', related_name='user_city', on_delete=models.CASCADE,blank=True,null=True)
+    number_phone = models.CharField(max_length=15)
+    image = models.ImageField(upload_to=image_upload)
+
+    def __str__(self):
+        return str(self.user)
+
+#########################################################################
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+#########################################################################
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignupUserForm
+from django.contrib.auth import authenticate, login
+from .models import Profile
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request,username=username,password=password)
+            login(request,user)
+            return redirect('accounts:profile')
+
+    else:
+        form = SignupUserForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request,'registration/signup.html',context)
+
+#####################################################################
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    context = {
+        'profile':profile,
+    }
+    return render(request,'accounts/profile.html',context)
+
+#####################################################################
+
+def profile_edit(request):
+    
+    return render(request,'accounts/profile_edit.html')
